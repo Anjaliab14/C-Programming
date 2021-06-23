16c1c86735e4a9d4a2147f9aaa8c7d3e8e719112
Anjaliab14 <82696958+Anjaliab14@users.noreply.github.com>
2021-06-23 13:11:27 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,10 @@
+// Program to print all ASCII values and their equivalent characters using a while loop.
+#include<stdio.h>
+int main()
+{
+  int i;
+  for(i=33;i<=77;i++)
+  {
+    printf("%d = %c\n",i,i);
+  }
+}
